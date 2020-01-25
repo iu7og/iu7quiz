93c07881bf8450ffb265a30f838a50efde6112cb
Romanov Alexey <gta.sanandreassamp@yandex.ru>
2020-01-26 00:45:23 +0300

    superfix..

--- bot/config.py
+++ bot/config.py
@@ -7,7 +7,6 @@
 
 #import os
 
-BOT_TOKEN = '1091403941:AAF9V0yM4NdoFl4oekUc4ksfs0BYLGC-lvE'
 #BOT_TOKEN = os.environ['TOKEN']
 HOST_NAME = "mongodb://alexey:061100@127.0.0.1:27017/iu7quiz-db"
 GROUPS = ["ИУ7-21Б", "ИУ7-22Б", "ИУ7-23Б", "ИУ7-24Б", "ИУ7-25Б", "ИУ7-26Б"]
