1db6442c52c4f1bc0f78c1ebbc42f96ace5ba807
Kononenko Sergey <sergkononenko@mail.ru>
2020-01-26 01:51:33 +0300

    Update docs

--- bot/config.py
+++ bot/config.py
@@ -7,9 +7,14 @@
 
 import os
 
+# Конфигурация серверной части бота.
 TOKEN = os.environ['TOKEN']
+
+# Конфигурация MongoDB.
 DB_NAME = os.environ['DB_NAME']
 DB_USER = os.environ['DB_USER']
 DB_PASS = os.environ['DB_PASS']
 HOST = f"mongodb://{DB_USER}:{DB_PASS}@127.0.0.1:27017/{DB_NAME}"
+
+# Конфигурация клиентской части бота.
 GROUPS = ["ИУ7-21Б", "ИУ7-22Б", "ИУ7-23Б", "ИУ7-24Б", "ИУ7-25Б", "ИУ7-26Б"]
