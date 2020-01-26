ded1573dce12861e98c6b070d75cb1436ebf5f21
Romanov Alexey <gta.sanandreassamp@yandex.ru>
2020-01-26 18:35:47 +0300

    ADD: buttons with questions by time

--- bot/dbinstances.py
+++ bot/dbinstances.py
@@ -6,15 +6,26 @@
       базе данных (документы), используемые ботом и их поля.
 """
 
-from mongoengine import Document, IntField, StringField
+from mongoengine import Document, IntField, StringField, ListField
 
 
 class Student(Document):
     """
-        Класс описывающий студента в БД.
+        Класс, описывающий студента в БД.
     """
 
     user_id = IntField(required=True)
     login = StringField(required=True, max_length=200, default="None")
     group = StringField(required=True, max_length=200, default="None")
     meta = {"allow_inheritance": True}
+
+
+class Question(Document):
+    """
+        Класс, описывающий вопрос и варианты ответа для него.
+    """
+
+    day = IntField(required=True)
+    text = StringField(required=True, max_length=300)
+    answers = ListField(StringField(required=True, max_length=100))
+    correct_answer = StringField(required=True, max_length=1)
