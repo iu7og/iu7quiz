2f09b43c3a5011090dc88d647ec1ed9e7f391d89
Pavel Perestoronin <p_perestoronin@mail.ru>
2020-01-28 14:20:17 +0300

    Update style and formula (also merge new develop)

--- bot/rating.py
+++ bot/rating.py
@@ -2,37 +2,59 @@ import config
 from dbinstances import Student, Question
 from math import exp
 
-# Расчет доли баллов за быстроту реакции
-# time - время ответа в часах
+
 def waiting_func(time: int):
+    """
+        Расчет доли баллов за быстроту реакции
+        time - время ответа в часах
+    """
+
     return exp(-config.HALF_WAITING_FACTOR * time)
 
-# Расчет доли баллов за быстроту ответа
-# time - время ответа в секундах
-# good_time - время ответа в секундах, за которое получаешь 90%
+
 def answr_func(time: int, good_time: int):
+    """
+        Расчет доли баллов за быстроту ответа
+        time - время ответа в секундах
+        good_time - время ответа в секундах, за которое получаешь 90%
+        (можно настроить не 90, заменив 9 на что-то другое (например, для получения
+        80% нужно заменить на 8))
+    """
+
     return 9 * good_time / (time + 9 * good_time)
 
-# Формула расчета суммарного кол-ва баллов за ответ (учитывающая все характеристики)
-# q_cmplx - сложность вопроса (question complexity)
-# t1 - время ожидания готовности
-# t2 - время ответа
-# good_t2 - хорошее время ответа (на 90%)
-# atmpt - номер попытки
+
 def formula(q_cmplx: int, t1: int, t2: int, atmpt: int, good_t2: int):
+    """
+        Формула расчета суммарного кол-ва баллов за ответ (учитывающая все характеристики)
+        q_cmplx - сложность вопроса (question complexity)
+        t1 - время ожидания готовности
+        t2 - время ответа
+        good_t2 - хорошее время ответа (на 90%)
+        atmpt - номер попытки
+    """
+
     return 100 / atmpt * (config.WAITING_FACTOR * waiting_func(t1) + config.ANSWR_TIME_FACTOR * answr_func(t2, good_t2)) * (1 - config.CMPLXITY_FACTOR * q_cmplx)
 
-# Расчет баллов для студента stdnt, при ответе на вопрос qstn, i-ый раз (по умолчанию - последний)
-def answer_summary(stdnt: Student, qstn: Question, i=-1):
-    return formula(qstn.first_to_answer / qstn.total_answers, stdnt.data[qstn.number]['right'][i][0], student.data[qstn.number]['right'][i][1], len(stdnt.data['right']) + stdnt.data['wrong'], qstn.best_time_to_answer)
 
-# Возвращает отсортированный массив кортежей-пар (nickname, summary)
+def answer_summary(stdnt: Student, qstn: Question, i=-1):
+    """
+        Расчет баллов для студента stdnt, при ответе на вопрос qstn, i-ый раз (по умолчанию - последний)
+    """
+
+    return formula(1 - qstn.first_to_answer / qstn.total_answers, stdnt.data[qstn.day]['right'][i][0], stdnt.data[qstn.day]['right'][i][1], i + 1 + stdnt.data["wrong"] if i != -1 else len(stdnt.data['right']) + stdnt.data['wrong'], qstn.best_time_to_answer)
+
+
 def get_rating():
+    """
+        Возвращает отсортированный массив кортежей-пар (nickname, summary)
+    """
+
     rating = dict()
     for student in Student.objects():
         summary = 0
         for question in Question.objects():
-            for i in range(len(student.data[question.number])):
+            for i in range(len(student.data[question.day]["right"])):
                 summary += answer_summary(student, question, i)
         rating[student.tg_login] = summary
-    return sorted(rating.items(), key=lambda x:x[1])
+    return sorted(rating.items(), key=lambda x: x[1])
