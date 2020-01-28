e066d2756d9829bb08e9b2fe1c8fc6a70d06cbbe
Pavel Perestoronin <p_perestoronin@mail.ru>
2020-01-28 15:49:44 +0300

    Update style

--- bot/rating.py
+++ bot/rating.py
@@ -24,42 +24,35 @@ def waiting_func(time_in_hours):
 def answr_func(time_in_secs, good_time):
     """
         Расчет доли баллов за быстроту ответа.
-        good_time - время ответа в секундах, за которое получаешь 90%.
-        (можно настроить не 90, заменив 9 на что-то другое (например, для получения
-        80% нужно заменить на 8)).
     """
 
     return 9 * good_time / (time_in_secs + 9 * good_time)
 
 
-def calculate_score(q_cmplx, waiting_time, answer_time, atmpt, good_answer_time):
+def calculate_score(q_complexity, waiting_time, answer_time, attempt, good_answer_time):
     """
         Формула расчета суммарного кол-ва баллов за ответ (учитывающая все характеристики).
-        q_cmplx - сложность вопроса (question complexity).
-        good_answer_time - хорошее время ответа (на 90%).
-        atmpt - номер попытки.
     """
 
     answer_score = (config.WAITING_FACTOR * waiting_func(waiting_time) +
                     config.ANSWER_TIME_FACTOR * answr_func(answer_time, good_answer_time))
-    complexity = (1 - config.COMPLEXITY_FACTOR * q_cmplx)
-    return 100 / atmpt * answer_score * complexity
+    complexity = (1 - config.COMPLEXITY_FACTOR * q_complexity)
+    return 100 / attempt * answer_score * complexity
 
 
 def answer_summary(student, question, answer_time=-1):
     """
         Расчет баллов для студента student, при ответе на вопрос question,
-        answer_time-ый раз (по умолчанию - последний).
     """
 
-    return calculate_score(
-        1 - question.first_to_answer / question.total_answers,
-        student.data[question.day]['right'][answer_time][0],
-        student.data[question.day]['right'][answer_time][1],
-        i + 1 + student.data["wrong"] if
-        i != -1 else len(student.data['right']) + student.data['wrong'],
-        question.best_time_to_answer
-    )
+    q_complexity = 1 - question.first_to_answer / question.total_answers,
+    waiting_time = student.data[question.day]['right'][answer_time][0]
+    time_of_answer = student.data[question.day]['right'][answer_time][1]
+    attempt = answer_time + 1 + student.data["wrong"] if \
+    answer_time != -1 else len(student.data['right']) + student.data['wrong']
+    good_answer_time = question.best_time_to_answer
+
+    return calculate_score(q_complexity, waiting_time, time_of_answer, attempt, good_answer_time)
 
 
 def get_rating():
