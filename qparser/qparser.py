d6d42ed4640960d930968bdbe94f8407f7d3bf55
Kononenko Sergey <sergkononenko@mail.ru>
2020-01-29 17:38:24 +0300

    Add questions parser due to #6

---
+++ qparser/qparser.py
@@ -0,0 +1,45 @@
+"""
+    ===== IU7QUIZ QUESTIONS PARSER =====
+    Copyright (C) 2020 IU7OG Team.
+
+    Модуль для парсинга вопросов по лекциям из текстового файла.
+"""
+
+
+import os
+import mongoengine
+from bot.config import DB_NAME, DB_USER, DB_PASS
+from bot.dbinstances import Question
+
+RECORD_SIZE = 6
+
+DB_IP = os.environ['DB_IP']
+HOST = f"mongodb://{DB_USER}:{DB_PASS}@{DB_IP}:27017/{DB_NAME}"
+
+
+def parse_questions():
+    """
+        Парсинг вопросов из файла.
+    """
+
+    mongoengine.connect(host=HOST)
+
+    dbquestions = len(Question.objects)
+
+    with open("./qparser/questions.txt") as file:
+        data = file.read().splitlines()
+
+    for i in range(0, len(data), RECORD_SIZE):
+        post = Question(
+            day=dbquestions + (i // RECORD_SIZE) + 1,
+            text=data[i % RECORD_SIZE],
+            answers=[data[j % RECORD_SIZE] for j in range(1, RECORD_SIZE - 1)],
+            correct_answer=data[(i + RECORD_SIZE - 1) % RECORD_SIZE]
+        )
+        post.save()
+
+    mongoengine.disconnect()
+
+
+if __name__ == "__main__":
+    parse_questions()
