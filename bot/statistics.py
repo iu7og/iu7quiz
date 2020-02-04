d26bf84ce95fcdcb40b2975714525607994769e2
Pavel Perestoronin <p_perestoronin@mail.ru>
2020-02-05 01:30:49 +0300

    Fix some bugs

--- bot/statistics.py
+++ bot/statistics.py
@@ -18,14 +18,12 @@ def ready_update(datastore, day, start_time):
     # то добавить словари для сегодняшнего дня и всех предыдущих.
     while len(datastore) <= day:
         datastore.append(dict())
+        # Проинициализируем словарь.
+        datastore[-1]["right"] = list()
+        datastore[-1]["wrong"] = list()
 
     # В качестве объекта рассматривается словарь, относящийся к сегодняшнему дню.
     question_object = datastore[day]
-
-    # Если словарь пуст (то есть был только что создан), то проинициализируем его.
-    if "wrong" not in question_object or "right" not in question_object:
-        question_object["wrong"] = list()
-        question_object["right"] = list()
 
     # Если словарь уже был проинициализирован (то есть это не первый ответ на данный вопрос),
     # то записать время реакции.
